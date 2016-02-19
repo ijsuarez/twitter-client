cc9d3ac699a5eb205f3ca1db7196b565e677c9a3
Isaias Suarez <ijsuarez@wustl.edu>
2016-02-18 19:13:36 -0600

    finished designing compose view, only thing left is to pass data around segues

--- TwitLite/ComposeViewController.swift
+++ TwitLite/ComposeViewController.swift
@@ -8,17 +8,77 @@
 
 import UIKit
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var placeholderText: UILabel!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var numChars: UILabel!
+    
+    var user: User!
+    var replyTo: Tweet?
+    let maxChars = 140
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        textView.delegate = self
+        textView.becomeFirstResponder()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        
+        if replyTo != nil {
+            placeholderText.hidden = true
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        textView.resignFirstResponder()
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        var info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomConstraint.constant = keyboardFrame.size.height
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomConstraint.constant = self.view.frame.minY
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        return textView.text.characters.count + (text.characters.count - range.length) <= maxChars
+    }
+    
+    func textViewDidChange(textView: UITextView) {
+        let charCount = textView.text.characters.count
+        if charCount > 0 {
+            placeholderText.hidden = true
+            numChars.text = "\(maxChars - charCount)"
+        } else {
+            placeholderText.hidden = false
+            numChars.text = "\(maxChars)"
+        }
+        
+    }
+    
+    @IBAction func onTweet(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            print("dismiss from tweet")
+        }
     }
     
     @IBAction func onExit(sender: AnyObject) {
